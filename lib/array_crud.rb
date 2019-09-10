7ef3854c7842f8ca44f8a7361659b389f82be787
Sarah H <sarahrose.henderson@gmail.com>
2019-09-10 16:33:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,8 @@ array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-
+array = ["element1", "element2", "element3", "element4"]
+array.pop
 end
 
 def remove_element_from_start_of_array(array)
