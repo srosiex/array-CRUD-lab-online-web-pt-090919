19e1c86f9726b518dd1d04bab5c2a91d7208d0de
Sarah H <sarahrose.henderson@gmail.com>
2019-09-10 16:34:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ end
 
 def remove_element_from_start_of_array(array)
 array = ["element1", "element2", "element3", "element4"]
-array.shift
+array.shift("arrays!")
 end
 
 def retrieve_element_from_index(array, index_number)
